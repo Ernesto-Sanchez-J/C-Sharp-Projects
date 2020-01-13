34887faf46300388c7945cdcb13e90b45f99119d
Ernesto-Sanchez-J <ernesto.j.sanchez12@gmail.com>
2020-01-12 21:25:27 -0600

    Finished hello world and addition apps

--- ManyMethods/ManyMethods/Program.cs
+++ ManyMethods/ManyMethods/Program.cs
@@ -4,16 +4,33 @@ namespace ManyMethods
 {
     class Program
     {
-        static void Main(string[] args) => Console.WriteLine("Hello, what is your name?");
-        Console.Readme(); 
-        Console.Writeline("bye {0)!, YourName);
+        static void Main(string[] args)
+        {
+            Hello();
+            Addition();
+        }
 
-        Console.Writeline("Enter first number");
-        int secondnumber = int.Parse(Console.ReadLine());
+        //HelloWorld app//
+        static void Hello()
+        {
 
-
+            Console.WriteLine("What, is your name?");
+            
+            string Name = Console.ReadLine();
+            Console.WriteLine("Goodbye, " + Name);
+            Console.ReadKey();
+        }
+        //Addition app, adds 2 numbers together//
+        static void Addition()
+        {
+            Console.WriteLine("Enter a number");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Enter another number");
+            string number2 = Console.ReadLine();
+            int number1Convert = Convert.ToInt32(number1);
+            int number2Convert = Convert.ToInt32(number2);
+            Console.WriteLine(number1Convert + number2Convert);
+        }
 
     }
-
-
 }
