bbc56140b51d191dec0b0808ad4286ebde0fc210
Ernesto-Sanchez-J <ernesto.j.sanchez12@gmail.com>
2020-01-12 19:39:29 -0600

    start

---
+++ ManyMethods/ManyMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManyMethods
+{
+    class Program
+    {
+        static void Main(string[] args) => Console.WriteLine("Hello, what is your name?");
+        Console.Readme(); 
+        Console.Writeline("bye {0)!, YourName);
+
+        Console.Writeline("Enter first number");
+        int secondnumber = int.Parse(Console.ReadLine());
+
+
+
+    }
+
+
+}
