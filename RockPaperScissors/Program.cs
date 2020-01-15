5a361e0cea18876aeb415ef9971345e20ef6fb74
Ernesto-Sanchez-J <ernesto.j.sanchez12@gmail.com>
2020-01-14 20:41:22 -0600

    did it work

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            input();
+            compare;
+        }
+            static void input()
+        {
+            Console.WriteLine("Let's play Rock Paper Scissors. Enter a R for Rock, enter a P for Paper, and enter a S for Scissors.");
+            Char userchoice = Console.Read();
+            
+            Random generator = new Random();
+            int computerchoice = generator.Next(0, 3);
+
+        }
+        static string compare()
+        {
+                static String CompareChoices(Char userchoice, int computerchoice);
+        }
+    }
+    
+}   
