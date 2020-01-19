3502aa01c507f0dddd67e4a3e2b925b8bfd5414a
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-01-18 23:00:10 -0600

    need branches

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -2,11 +2,55 @@
 
 namespace TicTacToe
 {
-    class Program
+    class TicTacToe
     {
-        static void Main(string[] args)
+        static int[,] board;
+        string playerName;
+
+        public TicTacToe(int player, string playerName)
         {
-            Console.WriteLine("Hello World!");
+            PlayerName = playerName
+            Player = player;
+            board = new int[3, 3];
+        }
+        public int Player { get; set;  }
+
+        public string PlayerName
+        {
+            get { return _playerName;  }
+            set
+            {
+                if (value.Length > 0)
+                    _playerName = value;
+                else
+                {
+                    while (value.Length < 1)  //has to be a name and not nothing
+                    {
+                        Console.WriteLine("InvalidCastException entry. Please re-enter player's name: ");
+                        value = Console.ReadLine();
+                    }
+                }
+            }
+        }
+        public static void intBoard()
+        {
+            for (int r =0; r < 3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                    board[r, c] = 0;
+            }
+            displayBoard();
+        }
+        private static void displayBoard()
+        {
+            for (int r = 0; r <3; r++)
+            {
+                for (int c = 0; c < 3; c++)
+                    Console.Write(board[r, c] + "");
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();  //extra line between moves
         }
     }
 }
