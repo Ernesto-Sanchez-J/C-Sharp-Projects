6481b71651275fc3634918b6fb198eb9743e5e88
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-01-30 20:13:47 -0600

    Need to add foreach loop on ToDo

--- TodoItems/Program.cs
+++ TodoItems/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TodoItems
 {
     class Program
     {
+        public static string Description;
+        public static string Date;
+        public static string Priority;
+        public static string Continue;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Your To Do app is ready for input.");
+            
+            Console.Write("Enter the description: ");
+            Description = Console.ReadLine();
+            Console.Write("Enter date due: ");
+            Date = Console.ReadLine();
+            Console.Write("Enter Priority, Low, Medium, High: ");
+            Priority = Console.ReadLine();
+            Console.WriteLine("Anymore inputs? yes or no");
+            Continue = Console.ReadLine();
+                if
+                {
+                Continue == no;
+                }
+                else
+                {
+
+                }
+
+                List<String> input = new List<String>();
+                {
+                input.Add(Description);
+                input.Add(Date);
+                input.Add(Priority);
+                }
+                
+
         }
+        
+
+
+
     }
 }
+      
+
