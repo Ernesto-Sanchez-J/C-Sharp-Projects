68ea2b5ba6ddaf249272c79dae5345214763c97f
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-02-24 16:20:52 -0600

    finished

---
+++ BooksInventory/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooksInventory
+{
+    class Book
+    {
+        public int Id { get; private set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Book(string Title, string Author)
+        {
+            this.Author = Author;
+            this.Title = Title;
+        }
+    }
+}
--- BooksInventory/Program.cs
+++ BooksInventory/Program.cs
@@ -6,7 +6,29 @@ namespace BooksInventory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BookContext context = new BookContext();
+            context.Database.EnsureCreated();
+            Console.WriteLine("Enter a Title and Author separated by a comma to be added.");
+            String UserBook = Console.ReadLine();
+
+            String[] parts = UserBook.Split(",");
+            if (parts.Length == 2)
+            {
+                Book book = new Book(parts[0], parts[1]);
+                context.books.Add(book);
+                context.SaveChanges();
+                Console.WriteLine("Added the book.");
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid title and/ or author, did not add book");
+            }
+            Console.WriteLine("The Current List of books are: ");
+            foreach(Book entry in context.books)
+            {
+                Console.WriteLine("{0} - {1}  | {2}", entry.Id, entry.Title, entry.Author);
+            }
         }
     }
 }
